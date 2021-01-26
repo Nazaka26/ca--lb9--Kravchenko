caa26b4c126b135ef40ea67c0dd29ec4e09ea6f3
Nazar Kravchenko <49519320+Nazaka26@users.noreply.github.com>
2021-01-26 20:14:35 +0200

    code

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+﻿#include "lb_9.h"
+
+void main()
+{
+	setlocale(LC_CTYPE, "rus");
+	
+	lb_9::display_exercise(1);
+	lb_9::display_exercise(2);
+	lb_9::display_exercise(3);
+	lb_9::display_exercise(4);
+	cout << endl << endl;
+	
+
+	system("pause");
+}
---
+++ lb_9.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class lb_9
+{
+public:
+	void static display_exercise(int num);
+private:
+	static void ex1();
+	static void ex2();
+	static void ex3();
+	static void ex4();
+};
